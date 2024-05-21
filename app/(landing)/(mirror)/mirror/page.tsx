70e4c69f6d5d749ab89f2a4c79e1f2f789811a6b
joseph-muchene <ngugimuchene@gmail.com>
2024-05-21 11:24:43 +0300

    javascript playground

--- app/(landing)/(mirror)/mirror/page.tsx
+++ app/(landing)/(mirror)/mirror/page.tsx
@@ -1,16 +1,62 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { javascript } from "@codemirror/lang-javascript";
+import CodeMirror from "@uiw/react-codemirror";
+import React from "react";
+
 function CodePage() {
+  const [value, setValue] = React.useState("Have fun writing javascript code;");
+  const [logs, setLogs] = React.useState<string[]>([]);
+
+  const onChange = React.useCallback((val: string, viewUpdate: any) => {
+    console.log("val:", val);
+    setValue(val);
+  }, []);
+
+  const runCode = React.useCallback(() => {
+    setLogs([]); // Clear previous logs
+
+    // Capture console.log outputs
+    const originalLog = console.log;
+    console.log = (...args) => {
+      setLogs((prevLogs) => [...prevLogs, args.join(" ")]);
+      originalLog(...args);
+    };
+
+    try {
+      // Use eval to execute the code
+      eval(value);
+    } catch (e: any) {
+      setLogs((prevLogs) => [...prevLogs, `Error: ${e.message}`]);
+    } finally {
+      // Restore the original console.log function
+      console.log = originalLog;
+    }
+  }, [value]);
+
   return (
     <>
-      <div className="container mt-[20px] overflow-auto">
-        <iframe
-          src="https://www.programiz.com/html/online-compiler/"
-          width="700"
-          height="800"
-        ></iframe>
+      <div className="flex flex-col  space-x-4 justify-around">
+        <div>
+          <CodeMirror
+            value={value}
+            theme={"dark"}
+            className="md:w-[60vw]"
+            extensions={[javascript({ jsx: true })]}
+            onChange={onChange}
+          />
+          <Button onClick={runCode} style={{ marginTop: "20px" }}>
+            Run Code
+          </Button>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <h3>Output:</h3>
+          <pre>{logs.join("\n")}</pre>
+        </div>
       </div>
     </>
   );
 }
+
 export default CodePage;
