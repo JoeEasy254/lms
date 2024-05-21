f697bf72130bbe251bd3620b10b3d25098f689fd
joseph-muchene <ngugimuchene@gmail.com>
2024-05-21 12:11:26 +0300

    some feartures on the create form (add new fields)

--- app/(landing)/(dashboard)/rooms/create/_components/Form-create.tsx
+++ app/(landing)/(dashboard)/rooms/create/_components/Form-create.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import axios from "axios";
+import { Calendar } from "@/components/ui/calendar";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -18,17 +20,25 @@ import { Input } from "@/components/ui/input";
 
 import { useRouter } from "next/navigation";
 import { toast } from "@/components/ui/use-toast";
+import { useState } from "react";
 
 const formSchema = z.object({
   name: z.string().min(2).max(50),
+  title: z.string().min(5).max(50),
+  description: z.string().min(5).max(50),
+  price: z.number(),
 });
 
 export const CreateForm = () => {
   const router = useRouter();
+  const [date, setDate] = useState<Date | undefined>(new Date());
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
+      title: "",
+      description: "",
+      price: 0,
     },
   });
   const { isSubmitting, isValid } = form.formState;
@@ -37,17 +47,17 @@ export const CreateForm = () => {
     // create a user
     try {
       const room = await axios.post("/api/room", { name: values.name });
-         toast({
-           title: "Alert",
-           description: "Room created",
-         });
+      toast({
+        title: "Alert",
+        description: "Room created",
+      });
       // router.push(`/rooms/join/${room.data.id}`);
     } catch (error) {
       console.log(error);
-        toast({
-          title: "Error",
-          description: "Something went wrong",
-        })
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+      });
     }
   }
   return (
@@ -68,6 +78,51 @@ export const CreateForm = () => {
               </FormItem>
             )}
           />
+
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Topic</FormLabel>
+                <FormControl>
+                  <Input placeholder="topic" {...field} />
+                </FormControl>
+                <FormDescription>
+                  A title on what will be taught.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="A brief description of the class"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Class description</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div>
+            <h1>Date</h1>
+
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              className="rounded-md border"
+            />
+          </div>
           <Button disabled={isSubmitting || !isValid} type="submit">
             Submit
           </Button>
--- app/(landing)/(dashboard)/rooms/create/page.tsx
+++ app/(landing)/(dashboard)/rooms/create/page.tsx
@@ -1,9 +1,10 @@
 import { CreateForm } from "./_components/Form-create";
 
 export default function CreateRoom() {
-
   return (
-    <div className="flex justify-center h-screen items-center">
+    <div className="mx-auto md:w-[600px]">
+      <h2 className="my-4 text-xl">Create your class</h2>
+
       <CreateForm />
     </div>
   );
